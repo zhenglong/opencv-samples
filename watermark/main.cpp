879dc55cbf0f21da2389dff19925d1695316f705
luozhenglong <luozhenglong@hujiang.com>
2019-10-29 13:38:58 +0800

    Initial Commit

---
+++ watermark/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  watermark
+//
+//  Created by HJ on 2019/10/29.
+//  Copyright © 2019 HJ. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
